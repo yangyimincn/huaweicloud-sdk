a3fccad5a6a7e294ce6c75cf1ceb37f9be9a60a5
yangyimincn <yangyimincn@163.com>
2020-03-27 15:03:52 +0800

    [feat]cph创建phone server（未测试）

--- services/cph/v1/create_cloud_phone_server.go
+++ services/cph/v1/create_cloud_phone_server.go
@@ -1,8 +1,33 @@
 package v1
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type CreateCloudPhoneServerResponse struct {
 	RequestID string `json:"request_id"`
 	OrderID   string `json:"order_id"`
 	ProductID string `json:"product_id"`
 }
 
+func (c *CPHClient) CreateCloudPhoneServer(body map[string]interface{}) (*CreateCloudPhoneServerResponse, error) {
+	uri := fmt.Sprintf("/v1/%s/cloud-phone/phones", c.GetProjectID())
+	res := CreateCloudPhoneServerResponse{}
+
+	bodyB, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := c.DoRequest("POST", uri, nil, bodyB)
+
+	if err != nil {
+		return &res, err
+	}
+	err = json.Unmarshal(result, &res)
+	if err != nil {
+		return &res, err
+	}
+	return &res, nil
+}
